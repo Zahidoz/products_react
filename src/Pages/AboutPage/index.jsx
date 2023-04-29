5d8e6f2bfa0398502a793e5493042341f0f5ac9f
Zahid Vahabzade <zahid.vahabzade@gmail.com>
2023-04-29 14:57:10 +0400

    products and product page structure

---
+++ src/Pages/AboutPage/index.jsx
@@ -0,0 +1,14 @@
+import Nav from "../../Components/Nav"
+
+const AboutPage = () => {
+  return (
+    <div>
+        <Nav/>
+        <h1>AboutPage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat veritatis illo ducimus harum nostrum adipisci excepturi vel iusto dolores, dolore maxime incidunt eum id sed ea corrupti ab laborum! Iure.</p>
+    </div>
+  )
+}
+
+export default AboutPage
+
